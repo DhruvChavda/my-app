240ce839aecb60962c3f9b6349e2e1b8adb5a09d
DMC2712 <chavdadhruv0505@gmail.com>
2021-05-13 01:31:37 +0530

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Just to check how to deploy a 'REACT' app on github
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://dhruvchavda.github.io/me"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          DMC
         </a>
       </header>
     </div>
